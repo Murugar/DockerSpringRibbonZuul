f1c6b270417fe61c0a521d5002362612178745be
Davanon <davanon2014@gmail.com>
2018-01-20 11:03:18 +0530

    First

---
+++ helloworld-service/src/main/java/com/iqmsoft/rest/Application.java
@@ -0,0 +1,18 @@
+package com.iqmsoft.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableEurekaClient
+@SpringBootApplication
+@ComponentScan("com.iqmsoft")
+public class Application {
+
+    public static void main(final String[] args) {
+      
+        SpringApplication.run(Application.class, args);
+    }
+
+}
